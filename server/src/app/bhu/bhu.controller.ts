052ddefd22fdcc63f491184adfecdf2c70dd31db
Genes Luna <genesluna@gmail.com>
2023-11-10 21:12:15 -0300

    added swagger operation

--- server/src/app/bhu/bhu.controller.ts
+++ server/src/app/bhu/bhu.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { BhuService } from './bhu.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-@ApiTags('basic health units')
+@ApiTags('Basic health units')
 @Controller('v1/bhus')
 export class BhuController {
   constructor(private readonly bhuService: BhuService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get basic health units list' })
   async getCities() {
     return await this.bhuService.getBhus();
   }
